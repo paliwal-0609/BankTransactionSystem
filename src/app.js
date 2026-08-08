38bdac88fcd398cc88bc92d9f93a828307320833
Dev Paliwal <devpaliwal7764@gmail.com>
2026-08-08 19:23:06 +0530

    Checking Deployment

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use(cookieParser());
 /** 
  * - Use Routes
 */
+
+app.get('/', (req, res)=>{
+    res.send('Ledger service is up and running')
+})
+
 app.use('/api/auth', authRouter);
 app.use('/api/accounts', accountRouter);
 app.use('/api/transactions', transactionRoutes);
