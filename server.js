9ccce1187e5ba7f4022f7dcd308e4d51fce9a394
Dev Paliwal <devpaliwal7764@gmail.com>
2026-08-08 19:16:40 +0530

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectDB = require('./src/config/db');
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000 || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
